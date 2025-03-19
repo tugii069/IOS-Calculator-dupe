aeeeab4e4db81d1a7785898ace4876fa328e3ee2
tugii069 <tugi1712@gmail.com>
2025-03-19 01:42:35 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tugay Kir on 19.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
